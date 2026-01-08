f1bbcee067303c6029ae3f54c8ae97e94a939521
Kamal-13-Sing <donbimal383@gmail.com>
2026-01-08 19:08:31 +0545

    resolve

--- UserService/UserService/HotelService/HotelService/src/main/java/com/lcwd/hotel/repositories/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lcwd.hotel.repositories;
-
-import com.lcwd.hotel.entities.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelRepository extends JpaRepository<Hotel, String> {
-}
