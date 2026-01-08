75267dd6cdd8ff35a7653b23b2b01dd118b95c32
Kamal-13-Sing <donbimal383@gmail.com>
2026-01-08 18:24:41 +0545

    commit user service

--- src/main/java/com/lcwd/user/service/services/UserServiceImp.java
+++ src/main/java/com/lcwd/user/service/services/UserServiceImp.java
@@ -1,11 +1,16 @@
 package com.lcwd.user.service.services;
 
+import com.lcwd.user.service.dtos.Rating;
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.exceptions.ResourceNotFoundException;
 import com.lcwd.user.service.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +19,9 @@ import java.util.UUID;
 public class UserServiceImp implements UserServices {
 
     private final UserRepository userRepository;
+
+    private final RestTemplate restTemplate;
+    private Logger logger = LoggerFactory.getLogger(UserServiceImp.class);
 
     @Override
     public User saveUser(User user) {
@@ -31,6 +39,17 @@ public class UserServiceImp implements UserServices {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id not found in server !! " + userId));
+
+        //get user from database using user repo
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id not found in server !! " + userId));
+
+        //fetch rating of the above user from rating services
+        // http://localhost:8083/ratings/users/65c55d01-dea9-4f83-8489-698a8bf4712d
+
+        ArrayList<Rating> ratingOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getUserId(), ArrayList.class);
+        logger.info("{}", ratingOfUser);
+
+        user.setRatings(ratingOfUser);
+        return user;
     }
 }
